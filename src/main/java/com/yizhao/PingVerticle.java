85e583c2a9397afa5a714abc61f9887f6ff34d9e
Yi Zhao <yizhao.cs@gmail.com>
2014-10-28 15:50:03 -0700

    vertx can be run

---
+++ src/main/java/com/yizhao/PingVerticle.java
@@ -0,0 +1,87 @@
+package com.yizhao;
+
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+/*
+ This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
+ */
+public class PingVerticle extends Verticle {
+	UploadBinaryDataAPI mUploadBinaryDataAPI;
+	DownloadBinaryDatAPI mDownloadBinaryDatAPI;
+
+	public void start() {
+
+//		vertx.eventBus().registerHandler("ping-address",
+//				new Handler<Message<String>>() {
+//					@Override
+//					public void handle(Message<String> message) {
+//						message.reply("pong!");
+//						container.logger().info("Sent back pong");
+//					}
+//				});
+		container.deployVerticle(PingVerticle.class.getCanonicalName(), 1);
+		// container.logger().info("PingVerticle started");
+//		vertx.createHttpServer()
+//				.requestHandler(new Handler<HttpServerRequest>() {
+//					public void handle(HttpServerRequest req) {
+//						String file = req.path().equals("/") ? "index.html"
+//								: req.path();
+//						req.response().sendFile("webroot/" + file);
+//					}
+//				}).listen(8080);
+		RouteMatcher httpRouteMatcher = new RouteMatcher();
+		HttpServer httpServer = vertx.createHttpServer();
+		httpServer.requestHandler(httpRouteMatcher);
+		httpServer.listen(8080, "0.0.0.0");
+		
+		httpRouteMatcher.post("/redis", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
+				mUploadBinaryDataAPI = new UploadBinaryDataAPI();
+				try {
+					container.logger().info("mUploadBinaryDataAPI");
+					mUploadBinaryDataAPI.upload();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		httpRouteMatcher.get("/redis", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
+				container.logger().info("mDownloadBinaryDatAPI");
+				mDownloadBinaryDatAPI = new DownloadBinaryDatAPI();
+				mDownloadBinaryDatAPI.download();
+			}
+		});
+
+	}
+}
